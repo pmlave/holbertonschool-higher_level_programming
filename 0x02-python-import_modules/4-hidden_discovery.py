aa82c58b8d14a53ab359d2de0f177bc0c619bd8d
pmlave <pmlave@gmail.com>
2018-09-19 17:11:39 +0000

    Updated answer to #4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-import hidden_4
-
-mylist = dir(hidden_4)
-for x in mylist:
-    if x[0:2] is "__":
-        pass
-    else:
-        print(x)
+if __name__ == "__main__":
+    import hidden_4
+    mylist = dir(hidden_4)
+    for x in mylist:
+        if x[0:2] is "__":
+            pass
+        else:
+            print(x)
