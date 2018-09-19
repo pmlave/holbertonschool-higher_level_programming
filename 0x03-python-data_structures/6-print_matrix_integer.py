063c79be28b6a544dca33c9a6adbfec7da2a6d3f
pmlave <pmlave@gmail.com>
2018-09-19 17:35:45 +0000

    Updated solution to #6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,4 +4,6 @@ def print_matrix_integer(matrix=[[]]):
     for x in range(len(matrix)):
         for y in range(len(matrix[x])):
             print("{:d}".format(matrix[x][y]), end="")
+            if y < (len(matrix[x]) - 1):
+                print(" ", end="")
         print("")
