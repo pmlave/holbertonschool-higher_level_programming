93a4ec3fd4af1fc0e88c164d12ab8d7be8b65c14
pmlave <pmlave@gmail.com>
2018-09-19 17:31:46 +0000

    New solution to #6

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+
+    for x in range(len(matrix)):
+        for y in range(len(matrix[x])):
+            print("{:d}".format(matrix[x][y]), end="")
+        print("")
