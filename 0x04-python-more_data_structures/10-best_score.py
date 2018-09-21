d85d1a697b6a22ee66239aa9c12c34b33d96e4a2
Paul Lave <pmlave@gmail.com>
2018-09-21 21:15:11 +0000

    Updated #10 once more

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is not None and a_dictionary is not {}:
+    if a_dictionary is not None and a_dictionary != {}:
         a = sorted(a_dictionary.keys())
         highest = 0
         for i in range(len(a_dictionary)):
