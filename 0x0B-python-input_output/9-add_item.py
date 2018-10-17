9b8db66a061ce75675bcfacc1285444eeaec31e4
Paul Lave <pmlave@gmail.com>
2018-10-17 05:44:56 +0000

    In progress solution for #9

---
+++ 0x0B-python-input_output/9-add_item.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+import sys
+import pathlib
+load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+
+filename = "add_item.json"
+try:
+    new_object = load_from_json_file(filename)
+    for i in range(len(sys.argv)):
+        if i == 0:
+            continue
+        new_object.append(sys.argv[i])
+        save_to_json_file(new_object, filename)
+except FileNotFoundError:
+    with open(filename, 'a+') as f:
+        for i in range(len(sys.argv)):
+            if i == 0:
+                continue
+            f.write(sys.argv[i])
+            save_to_json_file(new_object, filename)
