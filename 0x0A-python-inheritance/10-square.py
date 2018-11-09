8a64eb1b272d3a8adbf7f94d6adbbf4325bb7370
Paul Lave <pmlave@gmail.com>
2018-11-09 01:39:35 +0000

    Fixed size TypeError

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -4,9 +4,10 @@ Rectangle = __import__('9-rectangle').Rectangle
 
 class Square(Rectangle):
     def __init__(self, size):
-        Rectangle.__init__(self, size, size)
         self.integer_validator("size", size)
         self.__size = size
+        Rectangle.__init__(self, size, size)
+
 
     def area(self):
         return self.__size * self.__size
