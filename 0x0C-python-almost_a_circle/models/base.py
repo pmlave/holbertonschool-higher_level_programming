dd1668963ceaeea15dec0264d61c68d71117915e
Paul Lave <pmlave@gmail.com>
2018-11-13 01:48:42 +0000

    Documenting base class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
-
+"""Base for all other classes and methods"""
 
 
 
 
 class Base:
+    """Base class beginning"""
     __nb_objects = 0
 
     def __init__(self, id=None):
+        """Class constructor for base"""
         if id != None:
             self.id = id
         else:
