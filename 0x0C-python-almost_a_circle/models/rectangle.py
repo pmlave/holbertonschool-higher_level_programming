3be4fbfe1d324f8d4b5d55abebeac4d93f0937a6
Paul Lave <pmlave@gmail.com>
2018-11-14 18:56:56 +0000

    Fixed docstring errors

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
+'''New inheriting class for Base'''
 from models.base import Base
 
 
 class Rectangle(Base):
-
+    '''Rectangle class that inherits from base class'''
     def __init__(self, width, height, x=0, y=0, id=None):
+        '''init method that calls parent init'''
         super().__init__()
         if id is not None:
             self.id = id
@@ -15,10 +17,12 @@ class Rectangle(Base):
 
     @property
     def width(self):
+        '''Private width attribute getter'''
         return self.__width
 
     @width.setter
     def width(self, value):
+        '''Private width attribute setter'''
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
@@ -27,10 +31,12 @@ class Rectangle(Base):
 
     @property
     def height(self):
+        '''Private height attribute getter'''
         return self.__height
 
     @height.setter
     def height(self, value):
+        '''Private height attribute setter'''
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
@@ -39,10 +45,12 @@ class Rectangle(Base):
 
     @property
     def x(self):
+        '''Private x attribute getter'''
         return self.__x
 
     @x.setter
     def x(self, value):
+        '''Private x attribute setter'''
         if not isinstance(value, int):
             raise TypeError("x must be an integer")
         if value < 0:
@@ -51,10 +59,12 @@ class Rectangle(Base):
 
     @property
     def y(self):
+        '''Private y attribute getter'''
         return self.__y
 
     @y.setter
     def y(self, value):
+        '''Private y attribute setter'''
         if not isinstance(value, int):
             raise TypeError("y must be an integer")
         if value < 0:
