70530fd22cbb27c78606b6b81e02b47cfa364f6c
Paul Lave <pmlave@gmail.com>
2018-12-04 22:51:17 +0000

    Initial solution to #0 and new README

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,3 @@
+-- Listing all priveleges of the mysql users
+SHOW GRANTS FOR user_0d_1@localhost;
+SHOW GRANTS FOR user_0d_2@localhost;
---
+++ 0x0E-SQL_more_queries/README.md
@@ -0,0 +1,3 @@
+This project will take is more in depth into mysql.
+---
+0. Show all privileges of the users.
