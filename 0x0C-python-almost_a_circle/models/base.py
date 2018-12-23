71261add25926daff6f953fbdc7c1f11435bbd78
Paul Lave <pmlave@gmail.com>
2018-12-23 21:29:34 +0000

    Added empty list return to base model

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -44,6 +44,7 @@ class Base:
     @staticmethod
     def from_json_string(json_string):
         '''Returns the list of the Json string representation'''
+        empty = []
         if json_string is None or len(json_string) == 0:
-            return "[]"
+            return empty
         return json.loads(json_string)
