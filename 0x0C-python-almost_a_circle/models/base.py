73ef05248a33ed6bccfa1285cbd225b403694cb0
Paul Lave <pmlave@gmail.com>
2018-12-23 21:00:16 +0000

    New solution for #16

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 """Base for all other classes and methods"""
 
+import json
 
 class Base:
     """Base class beginning"""
     __nb_objects = 0
+    __file_path = "file.json"
 
     def __init__(self, id=None):
         """Class constructor for base"""
@@ -13,3 +15,11 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        '''Serialize the data into JSON'''
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        else:
+            for each_dict in list_dictionaries:
+                return json.dumps(each_dict)
