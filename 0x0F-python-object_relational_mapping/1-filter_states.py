197584438c4461eb4fcad8cf97cefcbc1a6aa1db
Paul Lave <pmlave@gmail.com>
2019-03-29 16:59:55 +0000

    Adding argument length checks and added BINARY to the like clause of the query on #1 and #2

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -5,15 +5,17 @@ import MySQLdb
 
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost",
-                         user=argv[1],
-                         passwd=argv[2],
-                         db=argv[3],
-                         port=3306)
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
-    cur.close()
-    db.close()
+    if len(argv) == 4:
+        db = MySQLdb.connect(host="localhost",
+                             user=argv[1],
+                             passwd=argv[2],
+                             db=argv[3],
+                             port=3306)
+        cur = db.cursor()
+        cur.execute("SELECT * FROM states WHERE name LIKE BINARY
+        'N%' ORDER BY id ASC")
+        rows = cur.fetchall()
+        for row in rows:
+            print(row)
+        cur.close()
+        db.close()
