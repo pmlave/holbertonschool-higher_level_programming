4db98decc572a34fb55ccbe3bfef98888a59d5bb
Paul Lave <pmlave@gmail.com>
2019-03-29 18:08:17 +0000

    Updated solutions to #10, #11, and #12 for pep8 style and code fixes

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     if len(argv) == 4:
         engine = create_engine(
             'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
-            argv[1], argv[2], argv[3]), pool_pre_ping=True)
+                argv[1], argv[2], argv[3]), pool_pre_ping=True)
 
         Session = sessionmaker(bind=engine)
         session = Session()
--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -10,12 +10,11 @@ if __name__ == "__main__":
     if len(argv) == 4:
         engine = create_engine(
             'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
-            argv[1], argv[2], argv[3]), pool_pre_ping=True)
+                argv[1], argv[2], argv[3]), pool_pre_ping=True)
 
         Session = sessionmaker(bind=engine)
         session = Session()
-        session.query(State).filter(State.id == 2).update({State.name: """
-        New Mexico
-        """}, synchronize_session=False)
+        session.query(State).filter(State.id == 2).update(
+            {State.name: """New Mexxico"""}, synchronize_session=False)
         session.commit()
         session.close()
